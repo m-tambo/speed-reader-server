9aefe39a0b65daf905f143125a4ccd715031db9b
Michael Tambornino <michael.tambornino@gmail.com>
2017-07-11 14:12:26 -0500

    adds express, cors, a port and a single endpoint

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4040;
+const cors = require('cors')
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
 
-nightmare
-  .goto('http://www.invivolink.com')
-  .evaluate( () => {
-    return {  start: window.performance.timing.navigationStart,
-              end: window.performance.timing.loadEventEnd
-            }
-  })
-  .end()
-  .then(res => {
-    console.log(res)
-  })
-  .catch( e => {
-    console.error('Search failed:', e);
-  });
+
+const getUrlStats = (url) => {
+  nightmare
+    .goto(url)
+    .evaluate( () => {
+      return {  navStart: window.performance.timing.navigationStart,
+                loadEventEnd: window.performance.timing.loadEventEnd,
+                requestStart: window.performance.timing.requestStart,
+                responseStart: window.performance.timing.responseStart,
+                pageSize: window.performance.memory.usedJSHeapSize
+              }
+    })
+    .end()
+    .then(res => {
+      console.log(`TTFB: ${(res.responseStart - res.requestStart)/1000} seconds`)
+      console.log(`Page load time: ${(res.loadEventEnd - res.navStart)/1000} seconds`)
+      console.log(`Page size: ${(res.pageSize)/10000000} MB`)
+      return res
+    })
+    .catch( e => {
+      console.error('Search failed:', e);
+    });
+}
+
+getUrlStats('http://www.hikarl.com')
+
+// allow cross origin sharing
+app.use(cors())
+
+app.get('/getstats/:url', (req, res) => {
+  res.send(getUrlStats(req.params.url))
+  console.log(`url to analyze: ${req.params.url} ...`)
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
