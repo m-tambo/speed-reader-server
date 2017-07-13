f43a3a82940df5898fcbf699242a344dabd09616
Michael Tambornino <michael.tambornino@gmail.com>
2017-07-12 23:10:43 -0500

    accept url from client, run nightmare scrape, and respond with metrics

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const port = process.env.PORT || 4040;
 const cors = require('cors')
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
+const expressValidator = require('express-validator')
 
 
-const getUrlStats = (url) => {
+// allow cross origin sharing
+app.use(cors())
+
+app.get('/getstats/:url', (req, res) => {
   nightmare
-    .goto(url)
+    .goto(`http://${req.params.url}`)
     .evaluate( () => {
       return {  navStart: window.performance.timing.navigationStart,
                 loadEventEnd: window.performance.timing.loadEventEnd,
@@ -18,27 +22,43 @@ const getUrlStats = (url) => {
               }
     })
     .end()
-    .then(res => {
-      console.log(`TTFB: ${(res.responseStart - res.requestStart)/1000} seconds`)
-      console.log(`Page load time: ${(res.loadEventEnd - res.navStart)/1000} seconds`)
-      console.log(`Page size: ${(res.pageSize)/10000000} MB`)
-      return res
+    .then( data => {
+      console.log(`data returned: ${data.pageSize}`)
+      res.status(200).json(data)
     })
     .catch( e => {
       console.error('Search failed:', e);
     });
-}
-
-getUrlStats('http://www.hikarl.com')
-
-// allow cross origin sharing
-app.use(cors())
-
-app.get('/getstats/:url', (req, res) => {
-  res.send(getUrlStats(req.params.url))
-  console.log(`url to analyze: ${req.params.url} ...`)
+  console.log(`url analyzed: ${req.params.url} ...`)
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 });
+
+
+// TODO refctor into stand-alone async/await
+// const getUrlStats = (url) => {
+//   nightmare
+//     .goto(url)
+//     .evaluate( () => {
+//       return {  navStart: window.performance.timing.navigationStart,
+//                 loadEventEnd: window.performance.timing.loadEventEnd,
+//                 requestStart: window.performance.timing.requestStart,
+//                 responseStart: window.performance.timing.responseStart,
+//                 pageSize: window.performance.memory.usedJSHeapSize
+//               }
+//     })
+//     .end()
+//     .then(res => {
+//       console.log(`TTFB: ${(res.responseStart - res.requestStart)/1000} seconds`)
+//       console.log(`Page load time: ${(res.loadEventEnd - res.navStart)/1000} seconds`)
+//       console.log(`Page size: ${(res.pageSize)/10000000} MB`)
+//       return res
+//     })
+//     .catch( e => {
+//       console.error('Search failed:', e);
+//     });
+// }
+//
+// getUrlStats('http://www.hikarl.com')
